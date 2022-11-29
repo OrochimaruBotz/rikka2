859339d8bdb3e6cf3193cf046c4b8b23a4c7f94c
Humanz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-11-29 21:01:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const ez = require("node-fetch");
-global.owner = ['62882003806038', '60102810046', '0'] // Put your number here
+global.owner = ['6288804947282', '6289697488609', '0'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -24,7 +24,7 @@ const choice = (array) => {
 return array[Math.floor(Math.random() * array.length)]
 }
 // Sticker WM
-global.packname = (`Rikka Bot`)
+global.packname = (`Gawr Gura Bot`)
 global.getBuffer = getBuffer
 global.author = (`×͜×  亗ᴮᵒᵗ`)
 global.sessionid = '48736705854:2Tq6joffmVDzaS:24'
